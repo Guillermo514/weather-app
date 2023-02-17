d2b42bf05b91fad085c40055de46f0c9269f28c9
Guillermo514 <125508192+Guillermo514@users.noreply.github.com>
2023-02-17 09:21:53 -0600

    new data forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,8 +3,6 @@ const request = require('request')
 const forecast = (latitud, longitud, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=933ae6eade8455e453acbf15a635abf0&query=' + latitud + ',' + longitud + '&units=m'
 
-    console.log(url)
-
     request({ url: url, json: true}, (error, {body}) => {
         if (error) {
             callback('No se pudo conectar con el Servidor de Clima', undefined)
@@ -16,7 +14,7 @@ const forecast = (latitud, longitud, callback) => {
             //     precip: body.current.precip,
             //     weather_descriptions: body.current.weather_descriptions
             // })
-            callback(undefined, 'Hay una temperatura de ' + body.current.temperature + ' Cº con una precipitacion de ' + body.current.precip + ' y un dia ' + body.current.weather_descriptions)
+            callback(undefined, 'Hay una temperatura de ' + body.current.temperature + ' Cº con una precipitacion de ' + body.current.precip + ' y un dia ' + body.current.weather_descriptions + '. Y hay una humedad de ' + body.current.humidity + '%.')
             // callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })

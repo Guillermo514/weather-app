34b3f89699316226d4cc6c12c58257cb16a3b77d
Guillermo514 <125508192+Guillermo514@users.noreply.github.com>
2023-02-21 16:42:30 -0600

    tuesday change
    2/21/23

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,18 +1,26 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-console.log('................')
+const id = new ObjectID()
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
-    
-    console.log('................')
-    
-    if(error){
-        return console.log('Error al conectar la base de datos')
+
+
+//mongoose.connect("paste db link", {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+MongoClient.connect(connectionURL, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
     }
 
-    console.log('Base de datos conectada')
+    const db = client.db(databaseName)
+
+    db.collection('task').deleteMany({
+        description: 'Azul'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 })
+
